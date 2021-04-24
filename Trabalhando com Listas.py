cc845c6ec7e5a5c02afdd69d5f0bcdd25a30a893
Guilherme Alves <guilherme.wott@gmail.com>
2021-04-24 17:41:05 -0300

    Update Trabalhando com Listas.py

--- Trabalhando com Listas.py
+++ Trabalhando com Listas.py
@@ -5,6 +5,7 @@ for c in range(0, 5):
 print(f'Os valores selecionados na lista foram: {list}')
 print(f'O maior valor selecionado foi: {max(list)} na posição {list.index(max(list))+1}')
 print(f'O menor valor selecionado foi: {min(list)} na posição {list.index(min(list))+1}')
+
 # Exercício 2
 list = []
 while True:
@@ -21,3 +22,44 @@ while True:
         break
 list.sort()
 print(f'Os números adicionados foram: {list}')
+
+# Exercício 3
+geral = []
+alunos = []
+while True:
+    alunos.append(str(input('Digite seu nome: ')).strip().capitalize())
+    alunos.append(float(input('Digite a nota do 1° bimestre: ')))
+    alunos.append(float(input('Digite a nota do 2° bimestre: ')))
+    alunos.append(float(input('Digite a nota do 3° bimestre: ')))
+    alunos.append(float(input('Digite a nota do 4° bimestre: ')))
+    alunos.append((alunos[1]+alunos[2]+alunos[3]+alunos[4])/4)
+    geral.append(alunos[:])
+    alunos.clear()
+    r = str(input('Deseja continuar? [S/N] ')).strip().upper()
+    while r not in 'SN':
+        print('Opção Inválida.')
+        r = str(input('Deseja continuar? [S/N] ')).strip().upper()
+    if r == 'N':
+        break
+print('=-'*13)
+print('{:^25}'.format(' MÉDIA DOS ALUNOS '))
+print('-='*13)
+print('{:<4}{}{:>20}'.format('Nº', 'Nome', 'Média'))
+for c in range(0, len(geral)):
+    print(f'{c:<2} {geral[c][0]:.<20} {geral[c][5]:.1f}')
+print('=-'*13)
+while True:
+    opç = str(input('Deseja analisar a nota de algum aluno? [S/N] ')).strip().upper()
+    while opç not in 'SN':
+        print('Opção Inválida.')
+        opç = str(input('Deseja analisar a nota de algum aluno? [S/N] ')).strip().upper()
+    if opç == 'N':
+        print('Programa Encerrado.')
+        break
+    n = int(input('Selecione o número correspondente ao aluno -> '))
+    print(f'''{geral[n][0]}
+Nota 1: {geral[n][1]}
+Nota 2: {geral[n][2]}
+Nota 3: {geral[n][3]}
+Nota 4: {geral[n][4]}
+Média final: {geral[n][3]:.1f}''')
